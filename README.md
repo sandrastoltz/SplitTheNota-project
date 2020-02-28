96b702846e246016c9c3d67b51a53f163ab74ef2
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2020-02-28 09:02:40 +0100

    Added code

---
+++ README.md
@@ -0,0 +1,5 @@
+# Övning split the nota
+
+## Instruktioner
+
+All HTML, CSS och Javascript som ni behöver finns. Er utmaning är att lägga all kod i index.js i rätt ordning så programmet funkar.
---
+++ js/index.js
@@ -0,0 +1,45 @@
+const tip;
+const sum;
+const numberOfFriends;
+const total;
+const friendSum;
+const sumDivided;
+const calculatedTip;
+const total; 
+
+
+document.getElementById('tip').value;
+
+function calculateTip(sum, tip) {}
+
+document.getElementById('calculateButton').addEventListener('click', function() {});
+
+document.getElementById('sum').value;
+
+calculateTip(sum, tip);
+
+parseInt(sum) + calculatedTip;
+
+function divideTotal(total, numberOfFriends) {}
+
+document.getElementById('friendSum').innerHTML = sum + ' kr';
+
+return sumDivided;
+
+showDividedSum(friendSum);
+
+document.getElementById('showSum').classList.toggle('hide');
+
+total / parseInt(numberOfFriends);
+
+function showDividedSum(sum) {}
+
+document.getElementById('inputForm').classList.toggle('hide');
+
+parseInt(sum) * parseFloat(tip);
+
+return total;
+
+divideTotal(total, numberOfFriends);
+
+document.getElementById('numberOfFriends').value;
